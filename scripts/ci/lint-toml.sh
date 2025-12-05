a01de863a49007d61ad928d9f53e5e21b5bab2d2
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-12-06 01:38:15 +0300

    chores: update python version, add uv sync to CI, update lint toml scripts
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- scripts/ci/lint-toml.sh
+++ scripts/ci/lint-toml.sh
@@ -3,5 +3,5 @@ echo "Lint the pyproject.toml"
 echo "Run taplo lint"
 taplo lint pyproject.toml
 echo "Run taplo format --check"
-taplo format --check
+taplo format --check pyproject.toml
 echo "OK"
