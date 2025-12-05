52be9abe9c6c1fa37f5f1e32abe568e39817b608
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-12-06 01:39:52 +0300

    chores: no commas in docstrings
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,9 +1,9 @@
-"""Tests for calculator."""
+"""Tests for calculator"""
 
 from cakeordog.calculator import get_five
 
 
 def test_get_five_returns_five() -> None:
-    """Check if function returns 5."""
+    """Check if function returns 5"""
     result = get_five()
     assert result == 5, f"Expected 5, received {result}"
