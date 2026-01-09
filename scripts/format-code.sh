e8ff72c038cf566340037f08fec6d9ee66e9b35f
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2026-01-09 13:06:22 +0300

    chores: apply analyzers on notebook
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- scripts/format-code.sh
+++ scripts/format-code.sh
@@ -1,7 +1,11 @@
 #!/bin/sh -e
 echo "Format code"
 echo "Running black"
-uv run black src/ tests/ docs/
+uv run black src/ tests/ docs/ notebook/
 echo "Running ruff --fix"
-uv run ruff check --fix src/ tests/ docs/
+uv run ruff check --fix src/ tests/ docs/ notebook/
+echo "Running black on notebook"
+uv run nbqa black notebook/
+echo "Running ruff --fix"
+uv run nbqa ruff --fix notebook/
 echo "OK"
