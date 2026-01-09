22b7f99fb95864e74c4adf4c9eab42a92202db37
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2026-01-09 13:06:22 +0300

    chores: apply analyzers on notebook
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- scripts/ci/lint-code.sh
+++ scripts/ci/lint-code.sh
@@ -6,4 +6,10 @@ echo "Running black --check"
 uv run black --check src/ tests/ docs/
 echo "Running mypy"
 uv run mypy src/ docs/
+echo "Running ruff on notebook"
+uv run nbqa ruff notebook/
+echo "Running black --check on notebook"
+uv run nbqa black --check notebook/
+echo "Running mypy on notebook"
+uv run nbqa mypy notebook/
 echo "OK"
